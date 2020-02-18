15cf25fbdefa5d0a5e79593effc37db7628df2d6
aynella <56473416+aynella@users.noreply.github.com>
2020-02-18 12:03:12 +0300

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,10 @@
+s="Abrakadabra"
+print(s[-9])
+print(s[9])
+print(s[0:5])
+print(s[0:9])
+print(s[0:11:2])
+print(s[1:11:2])
+print(s[::-1])
+print(s[::-2])
+print(len(s))
---
+++ d.py
@@ -0,0 +1,4 @@
+s=input()
+fw=s[:s.find(' ')]
+sw=s[s.find(' ')+1:]
+print(sw +' '+ fw)
